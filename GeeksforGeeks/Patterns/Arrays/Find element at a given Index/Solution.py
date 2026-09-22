c90efa40c22a766fc153f15fc18d02008c9ad557
Aryan Prasad <223308114+aryan-111106@users.noreply.github.com>
2026-09-22 22:28:31 +0530

    Delete GeeksforGeeks directory

--- GeeksforGeeks/Patterns/Arrays/Find element at a given Index/Solution.py
+++
@@ -1,5 +0,0 @@
-class Solution:
-    def findElementAtIndex(self, i, arr):
-        return arr[i]
-        
-    
--- GeeksforGeeks/Patterns/Arrays/Sum of Array/Solution.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-	def arraySum(self, arr):
-   		sum = 0
-   		for i in arr:
-   		    sum += i
-   	    return sum
-   	    
