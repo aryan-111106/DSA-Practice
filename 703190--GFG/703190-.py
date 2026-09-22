1b790488cc10672f0a2cab094ee88e7ebbdfc083
Aryan Prasad <223308114+aryan-111106@users.noreply.github.com>
2026-09-22 23:44:49 +0530

    Added solution

---
+++ 703190--GFG/703190-.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isSorted(self, arr):
+        for i in range(len(arr)-1):
+            if arr[i]>arr[i+1]:
+                return False
+        return True
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
