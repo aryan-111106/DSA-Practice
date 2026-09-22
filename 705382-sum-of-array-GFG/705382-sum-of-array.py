15edb37f607eab7708e2724c948c477d5ebf4713
Aryan Prasad <223308114+aryan-111106@users.noreply.github.com>
2026-09-22 22:36:49 +0530

    Force committed solution

---
+++ 705382-sum-of-array-GFG/705382-sum-of-array.py
@@ -0,0 +1,11 @@
+class Solution:
+	def arraySum(self, arr):
+   		sum = 0
+   		for i in arr:
+   		    sum += i
+   	    return sum
+   	    
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
